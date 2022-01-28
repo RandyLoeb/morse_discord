e0a08564d14003c93ee8210b34e331cf65c2e2f4
Randy Loeb <githubrandyloeb@randonloeb.com>
2022-01-28 16:05:32 -0500

    minor cleanup

--- src/bot.js
+++ src/bot.js
@@ -1,28 +1,15 @@
-//require("dotenv").config();
 import * as DotEnv from 'dotenv';
-DotEnv.config();
-
 import fs from 'fs';
-
 import { Client, WebhookClient } from 'discord.js';
-import { getVoiceConnection, joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice';
-
 import MorseCWWave from './morse-pro/morse-pro-cw-wave.js';
-
 import * as RiffWave from './morse-pro/morse-pro-util-riffwave.js';
-import { isTypedArray } from 'util/types';
 
+DotEnv.config();
 
 const client = new Client({ 
   partials: ['MESSAGE', 'REACTION']
 });
 
-const webhookClient = new WebhookClient(
-  process.env.WEBHOOK_ID,
-  process.env.WEBHOOK_TOKEN,
-);
-
-const PREFIX = "$";
 
 client.on('ready', () => {
   console.log(`${client.user.tag} has logged in.`);
@@ -36,6 +23,8 @@ client.on('message', async (message) => {
   var morseCWWave = new MorseCWWave();
   morseCWWave.translate(message.content);
   var wav = RiffWave.getData(morseCWWave.getSample());  
+
+  //POC we save to physical file
   fs.writeFileSync('test.wav',new Int8Array( wav));
   const voiceChannel = message.member.voice.channel;
   if (!voiceChannel) {
@@ -43,97 +32,12 @@ client.on('message', async (message) => {
   } else {
       console.log("in a voice channel");
   }
-   //   "You need to be in a voice channel to play music!"
-    
-  /*const connection = getVoiceConnection(message.member.voice.guild.id); */
-  var connection = voiceChannel.join();
   
+    var connection = voiceChannel.join();
+  
+  //POC play the physical file. TODO: skip need for physical file  
   (await connection).play('test.wav');
-  //var x = joinVoiceChannel(message.member.voice.channel);
-  /*
-  if (message.content.startsWith(PREFIX)) {
-    const [CMD_NAME, ...args] = message.content
-      .trim()
-      .substring(PREFIX.length)
-      .split(/\s+/);
-    if (CMD_NAME === 'kick') {
-      if (!message.member.hasPermission('KICK_MEMBERS'))
-        return message.reply('You do not have permissions to use that command');
-      if (args.length === 0)
-        return message.reply('Please provide an ID');
-      const member = message.guild.members.cache.get(args[0]);
-      if (member) {
-        member
-          .kick()
-          .then((member) => message.channel.send(`${member} was kicked.`))
-          .catch((err) => message.channel.send('I cannot kick that user :('));
-      } else {
-        message.channel.send('That member was not found');
-      }
-    } else if (CMD_NAME === 'ban') {
-      if (!message.member.hasPermission('BAN_MEMBERS'))
-        return message.reply("You do not have permissions to use that command");
-      if (args.length === 0) return message.reply("Please provide an ID");
-      try {
-        const user = await message.guild.members.ban(args[0]);
-        message.channel.send('User was banned successfully');
-      } catch (err) {
-        console.log(err);
-        message.channel.send('An error occured. Either I do not have permissions or the user was not found');
-      }
-    } else if (CMD_NAME === 'announce') {
-      console.log(args);
-      const msg = args.join(' ');
-      console.log(msg);
-      webhookClient.send(msg);
-    }
-  }
-  */
+  
 });
-
-/*
-client.on('messageReactionAdd', (reaction, user) => {
-  const { name } = reaction.emoji;
-  const member = reaction.message.guild.members.cache.get(user.id);
-  if (reaction.message.id === '738666523408990258') {
-    switch (name) {
-      case '🍎':
-        member.roles.add('738664659103776818');
-        break;
-      case '🍌':
-        member.roles.add('738664632838782998');
-        break;
-      case '🍇':
-        member.roles.add('738664618511171634');
-        break;
-      case '🍑':
-        member.roles.add('738664590178779167');
-        break;
-    }
-  }
-});
-
-client.on('messageReactionRemove', (reaction, user) => {
-  const { name } = reaction.emoji;
-  const member = reaction.message.guild.members.cache.get(user.id);
-  if (reaction.message.id === '738666523408990258') {
-    switch (name) {
-      case '🍎':
-        member.roles.remove('738664659103776818');
-        break;
-      case '🍌':
-        member.roles.remove('738664632838782998');
-        break;
-      case '🍇':
-        member.roles.remove('738664618511171634');
-        break;
-      case '🍑':
-        member.roles.remove('738664590178779167');
-        break;
-    }
-  }
-});
-
-*/
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
